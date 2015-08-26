98fe2642ac046343b65e8bf7f62adbf1968ad397
Alexandr Zotkin <oxionaz@gmail.com>
2015-08-26 15:46:31 +0300

    Delete string

--- app/src/main/java/ru/loftschool/loftblogmoneytracker/MainActivity.java
+++ app/src/main/java/ru/loftschool/loftblogmoneytracker/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.d(LOG_TAG, "onCreate method called!");
     }
 
